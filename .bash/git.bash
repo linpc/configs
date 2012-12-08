842540de1ad83ff07bdaf99eda79c5309b36cfa2
linpc <linpc@cs.nctu.edu.tw>
2012-12-08 18:06:33 +0800

    Config updates.

--- .bash/git.bash
+++ .bash/git.bash
@@ -4,7 +4,11 @@ function git_branch
     ref=$(git symbolic-ref HEAD 2> /dev/null)
     if [ -z "${ref}" ]; then
 	ref=$(git branch -a 2> /dev/null | grep '^*') || return;
-	echo "${ref#* }";
+	br="${ref#* }";
+	if [ "${br}" = '(no branch)' ];then
+	    head=$(git rev-parse HEAD | cut -c1-7)
+	    echo "(no branch:${head})"
+	fi
 	return
     fi
     echo "("${ref#refs/heads/}")";
@@ -12,12 +16,12 @@ function git_branch
 
 function git_since_last_commit
 {
-  now=`date +%s`;
-  last_commit=$(git log --pretty=format:%at -1 2> /dev/null) || return;
-  seconds_since_last_commit=$((now-last_commit));
-  minutes_since_last_commit=$((seconds_since_last_commit/60));
-  hours_since_last_commit=$((minutes_since_last_commit/60));
-  minutes_since_last_commit=$((minutes_since_last_commit%60));
+    now=`date +%s`;
+    last_commit=$(git log --pretty=format:%at -1 2> /dev/null) || return;
+    seconds_since_last_commit=$((now-last_commit));
+    minutes_since_last_commit=$((seconds_since_last_commit/60));
+    hours_since_last_commit=$((minutes_since_last_commit/60));
+    minutes_since_last_commit=$((minutes_since_last_commit%60));
 
   if [ ${hours_since_last_commit} -le 24 ]; then
       echo " ${hours_since_last_commit}h${minutes_since_last_commit}m ";
